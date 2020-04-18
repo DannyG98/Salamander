67ab80e1bb2e01476edfb1057500fd54f818fb56
Swunci <37261390+Swunci@users.noreply.github.com>
2020-04-18 01:56:34 -0400

    (Frontend) Implemented modify precinct functionality Added functionality to done button such that changes made during insert/modify precincts is saved Added functionality to cancel button such that all changes are discarded during past session of insert/modify precincts

--- src/main/resources/static/scripts/toolbar.js
+++ src/main/resources/static/scripts/toolbar.js
@@ -14,8 +14,7 @@ var ToolBar = {
 
     initStateDropdown: function() {
         // Add an event handler to move map to the selected state
-        var filtersContainer = document.getElementById("states");
-        var states = filtersContainer = filtersContainer.getElementsByClassName("dropdown-item");
+        var states = $('#states').find('.dropdown-item');
         for (var i = 0; i  < states.length; i++) {
             states[i].addEventListener("click", function() {
                 var state = this.text;
@@ -26,12 +25,11 @@ var ToolBar = {
 
     initElectionDropdown: function() {
         // Add event listeners to election options to handle updating of election data
-        var filtersContainer = document.getElementById("elections");
-        var elections = filtersContainer.getElementsByClassName("dropdown-item");
+        var elections = $('#elections').find('.dropdown-item');
         for (var i = 0; i  < elections.length; i++) {
             elections[i].addEventListener("click", function() {
                 // Change highlighted election to the selected one
-                var currentElection = filtersContainer.getElementsByClassName("active")[0];
+                var currentElection = $('#elections').find(".active")[0];
                 currentElection.className = currentElection.className.replace("active", "");
                 this.className += " active";
                 // TODO
@@ -45,9 +43,7 @@ var ToolBar = {
              // TODO
             // Toggle the visibility of the indicated element
         };
-
-        var filtersContainer = document.getElementById("filters");
-        var options = filtersContainer.getElementsByClassName("dropdown-item");
+        var options = $('#filters').find('.dropdown-item');
         for (var i = 0; i  < options.length; i++) {
             options[i].addEventListener("click", function() {
                 // Highlights filters dropdown menu elements when active
@@ -72,25 +68,29 @@ var ToolBar = {
     },
 
     insertPrecinctHandler: function() {
-        //toggleMode("insert")
         $('#insert').click( function() {
+            // Go to precinct level
+            LeafletMap.map.setZoom(8);
+            // Change mode to enable different map functionality
             LeafletMap.currentMode = LeafletMap.modes.insert;
             ToolBar.toggleEditButtons();
 
             LeafletMap.map.pm.enableDraw('Polygon');
+            // Limit zoom level to precinct level 
+            LeafletMap.map.options.minZoom = 8;
 
-            // Drawing eventHandlers
+            // EventHandler for when a precinct is created
             LeafletMap.map.on('pm:create', e => {
-                console.log('Drawing end');
                 LeafletMap.map.removeLayer(e.layer);
-
-                var precinctShape = "";
-                var precinctCoordinates = [];
-                var coordinatesList = e.layer._latlngs;
+                var precinctShape = e.shape;
+                var newPrecinctCoordinates = [];
+                var coordinatesList = e.layer._latlngs[0];
                 for (var i = 0; i < coordinatesList.length; i++) {
-                    precinctCoordinates.push([coordinatesList[i].lng, coordinatesList[i].lat])
+                    newPrecinctCoordinates.push([coordinatesList[i].lng, coordinatesList[i].lat])
                 };
-                coloradoPrecincts[0].features.push( 
+                
+                // Add new precinct boundaries to a temporary data structure to display
+                LeafletMap.tempPrecinctBoundaries.push( 
                 {
                     "type": "Feature",
                     "properties": 
@@ -107,13 +107,16 @@ var ToolBar = {
                     "geometry": 
                     {
                         "type": precinctShape,
-                        "coordinates": [[...precinctCoordinates]]
+                        "coordinates": [newPrecinctCoordinates]
                     }
                 });
-                if (LeafletMap.map.hasLayer(LeafletMap.precinctGeojson)) {
-                    LeafletMap.map.removeLayer(LeafletMap.precinctGeojson);
-                }
-                LeafletMap.precinctGeojson = L.geoJson(coloradoPrecincts,{ onEachFeature: LeafletMap.onEachFeature }).addTo(LeafletMap.map);
+                // Remove current temp geojson if it exists
+                if (LeafletMap.map.hasLayer(LeafletMap.tempGeojson)) { LeafletMap.map.removeLayer(LeafletMap.tempGeojson);}
+                // Add the new updated temp geojson
+                LeafletMap.tempGeojson = L.geoJson(LeafletMap.tempPrecinctBoundaries,{ onEachFeature: LeafletMap.onEachFeature }).addTo(LeafletMap.map);
+
+                // Allow user to draw more than one precinct
+                LeafletMap.map.pm.enableDraw('Polygon');
             });
         });
     },
@@ -124,18 +127,34 @@ var ToolBar = {
             ToolBar.toggleEditButtons();
         });
     },
+
     modifyPrecinctHanlder: function() {
         $('#modify').click( function() {
+            // Go to precinct level
+            LeafletMap.map.setZoom(8);
+
+            // Zooming recreates the precinct layers so must wait for precinctGeojson to be updated first
+            setTimeout (function () {
+                // Allow precinct borders to be edited
+                LeafletMap.precinctGeojson.pm.enable();
+            }, 300);
+            // Switch map mode
             LeafletMap.currentMode = LeafletMap.modes.modify;
             ToolBar.toggleEditButtons();
+
+            // Limit zoom level to precinct level 
+            LeafletMap.map.options.minZoom = 8;
+
         });
     },
+
     createCommentHandler: function() {
         $('#create').click( function() {
             LeafletMap.currentMode = LeafletMap.modes.create;
             ToolBar.toggleEditButtons();
         });
     },
+
     resetMapHandler: function() {
         $('#reset').click( function() 
         {
@@ -145,15 +164,56 @@ var ToolBar = {
 
     editButtonHandler: function() {
         $('#done-btn').click( function() {
+            // TODO
+            // Send updated data to server
+
+            switch(LeafletMap.currentMode) {
+                case LeafletMap.modes.insert: 
+                    // Add all the new precincts to the current list of precincts
+                    for (var i = 0; i < LeafletMap.tempPrecinctBoundaries.length; i++) {
+                        coloradoPrecincts.push(LeafletMap.tempPrecinctBoundaries[i]);
+                    }
+                    // Update the GUI
+                    if (LeafletMap.map.hasLayer(LeafletMap.precinctGeojson)) { 
+                        LeafletMap.map.removeLayer(LeafletMap.precinctGeojson); 
+                        LeafletMap.precinctGeojson = L.geoJson(coloradoPrecincts, { onEachFeature: LeafletMap.onEachFeature }, { style: { pmIgnore: false } }).addTo(LeafletMap.map);
+                    }
+                    break;
+                case LeafletMap.modes.modify:
+                    // Replace precinctCoordinates with the new ones from the precinctGeojson layer
+                    for (var i in LeafletMap.precinctGeojson._layers) {
+                        var precinctName = LeafletMap.precinctGeojson._layers[i].feature.properties.name;
+                        for (var j in coloradoPrecincts) {
+                            if (precinctName == coloradoPrecincts[j].properties.name) {
+                                console.log("Update the coordinates");
+                                var newPrecinctCoordinates = [];
+                                var coordinatesList = LeafletMap.precinctGeojson._layers[i]._latlngs[0];
+                                for (var k in coordinatesList) {
+                                    newPrecinctCoordinates.push([coordinatesList[k].lng, coordinatesList[k].lat]);
+                                };
+                                coloradoPrecincts[j].geometry.type = LeafletMap.precinctGeojson._layers[i].feature.geometry.type;
+                                coloradoPrecincts[j].geometry.coordinates = [newPrecinctCoordinates];
+                                break;
+                            }
+                        }
+                    };
+
+    
+                    break;
+                default:
+                    console.log("INVALID CURRENT MODE");
+            }
+
+
+            // Reset map functionalities
+            ToolBar.resetMapFunctionalities();
 
         });
         $('#cancel-btn').click( function() {
             // TODO
             // Revert all changes made during edit
+            ToolBar.resetMapFunctionalities();
 
-            // Hide the edit buttons
-            LeafletMap.currentMode = LeafletMap.modes.default;
-            ToolBar.toggleEditButtons();
         });
     },
 
@@ -166,10 +226,36 @@ var ToolBar = {
             $('#done-btn').show();
             $('#cancel-btn').show();
         }
+    },
 
+    resetMapFunctionalities: function() {
+        // Reset all temp variables and revert back to normal map functionality
+        switch(LeafletMap.currentMode) {
+            case LeafletMap.modes.insert:
+                // Need to remove all tempJson precincts from map and empty tempPrecinctBoundaries
+                if (LeafletMap.map.hasLayer(LeafletMap.tempGeojson)) { LeafletMap.map.removeLayer(LeafletMap.tempGeojson);}
+                LeafletMap.tempPrecinctBoundaries = [];
+                LeafletMap.tempGeojson = null;
+                // Refresh the precinct layers
+                if (LeafletMap.map.hasLayer(LeafletMap.precinctGeojson)) { 
+                    LeafletMap.map.removeLayer(LeafletMap.precinctGeojson);
+                    LeafletMap.precinctGeojson = L.geoJson(coloradoPrecincts, { onEachFeature: LeafletMap.onEachFeature }, { style: { pmIgnore: false } }).addTo(LeafletMap.map);
+                }
+                LeafletMap.map.pm.disableDraw();
+                break;
+            case LeafletMap.modes.modify:
+                if (LeafletMap.map.hasLayer(LeafletMap.precinctGeojson)) { 
+                    LeafletMap.map.removeLayer(LeafletMap.precinctGeojson);
+                    LeafletMap.precinctGeojson = L.geoJson(coloradoPrecincts, { onEachFeature: LeafletMap.onEachFeature }, { style: { pmIgnore: false } }).addTo(LeafletMap.map);
+                }
+                break;
+            default:
+                console.log("INVALID CURRENT MODE");
+        }
+        LeafletMap.currentMode = LeafletMap.modes.default;
+        LeafletMap.map.options.minZoom = 5;
+        ToolBar.toggleEditButtons();
     }
-
-
 };
 
 ToolBar.init();
