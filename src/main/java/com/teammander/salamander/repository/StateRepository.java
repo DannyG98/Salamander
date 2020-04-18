d682066ab06e7d6ca7db9b5b975bc439499d7ff5
Jason Cao <>
2020-04-18 15:33:19 -0400

    [Stubbed State Data] added in stubbed state coordinates for getAllStates on load in for sunday's assignment

--- src/main/java/com/teammander/salamander/repository/StateRepository.java
+++ src/main/java/com/teammander/salamander/repository/StateRepository.java
@@ -28,7 +28,7 @@ public class StateRepository {
                 new Coordinate(-102.042974, 36.994786), new Coordinate(-103.001438, 37.000263), new Coordinate(-104.337812, 36.994786), new Coordinate(-106.868158, 36.994786), new Coordinate(-107.421329, 37.000263),
                 new Coordinate(-109.042503, 37.000263), new Coordinate(-109.042503, 38.166851), new Coordinate(-109.058934, 38.27639), new Coordinate(-109.053457, 39.125316), new Coordinate(-109.04798, 40.998429), new Coordinate(-107.919731, 41.003906));
         DemographicData coDemo= new DemographicData(1, 10,12,16,40, 15);
-        State colorado = new State("01", "Colorado", coloradoBoundaries, coDemo, null, null);
+        State colorado = new State("colorado", "Colorado", coloradoBoundaries, coDemo, null, null);
 
 
         List<Coordinate> floridaBoundaries= Arrays.asList(new Coordinate(-85.497137,30.997536),new Coordinate(-85.004212,31.003013),new Coordinate(-84.867289,30.712735),new Coordinate(-83.498053,30.647012),new Coordinate(-82.216449,30.570335),new Coordinate(-82.167157,30.356734),
@@ -43,7 +43,7 @@ public class StateRepository {
                 new Coordinate(-84.451041,29.929533),new Coordinate(-84.867289,29.743317),new Coordinate(-85.310921,29.699501),new Coordinate(-85.299967,29.80904),new Coordinate(-85.404029,29.940487),new Coordinate(-85.924338,30.236241),new Coordinate(-86.29677,30.362211),
                 new Coordinate(-86.630863,30.395073),new Coordinate(-86.910187,30.373165),new Coordinate(-87.518128,30.280057),new Coordinate(-87.37025,30.427934),new Coordinate(-87.446927,30.510088),new Coordinate(-87.408589,30.674397),new Coordinate(-87.633143,30.86609),new Coordinate(-87.600282,30.997536),new Coordinate(-85.497137,30.997536));
         DemographicData flDemo= new DemographicData(2, 11,1,60,4, 15);
-        State florida = new State("02", "Florida", floridaBoundaries, flDemo, null, null);
+        State florida = new State("florida", "Florida", floridaBoundaries, flDemo, null, null);
 
 
         List<Coordinate> marylandBoundaries= Arrays.asList(new Coordinate(-79.477979,39.722302),new Coordinate(-75.786521,39.722302),new Coordinate(-75.693413,38.462606),new Coordinate(-75.047134,38.451652),new Coordinate(-75.244304,38.029928),new Coordinate(-75.397659,38.013497),
@@ -55,7 +55,7 @@ public class StateRepository {
                 new Coordinate(-77.117418,38.933623),new Coordinate(-77.248864,39.026731),new Coordinate(-77.456988,39.076023),new Coordinate(-77.456988,39.223901),new Coordinate(-77.566527,39.306055),new Coordinate(-77.719881,39.322485),new Coordinate(-77.834897,39.601809),new Coordinate(-78.004682,39.601809),
                 new Coordinate(-78.174467,39.694917),new Coordinate(-78.267575,39.61824),new Coordinate(-78.431884,39.623717),new Coordinate(-78.470222,39.514178),new Coordinate(-78.765977,39.585379),new Coordinate(-78.963147,39.437501),new Coordinate(-79.094593,39.470363),new Coordinate(-79.291763,39.300578), new Coordinate(-79.488933,39.20747),new Coordinate(-79.477979,39.722302));
         DemographicData mlDemo= new DemographicData(3, 21,6,61,4, 15);
-        State maryland = new State("03", "Maryland", marylandBoundaries, mlDemo, null, null);
+        State maryland = new State("maryland", "Maryland", marylandBoundaries, mlDemo, null, null);
 
         allStates.add(colorado);
         allStates.add(florida);
